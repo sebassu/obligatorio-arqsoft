ef1c02a9e1c52fdea69ba46affe86c9cc700b5cd
pabmor <pabmorajau@gmail.com>
2018-06-21 15:20:42 -0200

    SupplyOrderResourse: Neat & Tidy

--- roi-supplying-war/src/java/com/roi/supplying/SupplyOrderResource.java
+++ roi-supplying-war/src/java/com/roi/supplying/SupplyOrderResource.java
@@ -11,13 +11,14 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("supply-order")
 public class SupplyOrderResource {
-    
+
     @EJB
     private SupplyOrderBean supplyOrderBean;
-    
+
     @Context
     private UriInfo context;
     private final Gson gson;
@@ -25,26 +26,55 @@ public class SupplyOrderResource {
     public SupplyOrderResource() {
         this.gson = new Gson();
     }
-    
+
+    //TODO catch exceptions thrown by bean
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
-    public void createSupplyOrder(String content) {
+    public Response createSupplyOrder(String content) {
+        Response response;
         SupplyOrder supplyOrder = gson.fromJson(content, SupplyOrder.class);
-        supplyOrderBean.create(supplyOrder);
+        if (supplyOrder == null) {
+            response = Response.status(Response.Status.BAD_REQUEST)
+                    .entity("Invalid Supply Order provided")
+                    .build();
+        } else {
+            supplyOrderBean.create(supplyOrder);
+            response = Response.ok()
+                    .entity(supplyOrder)
+                    .type(MediaType.APPLICATION_JSON)
+                    .build();
+        }
+        return response;
     }
-    
+
     @PUT
     @Path("{id}")
     @Consumes(MediaType.APPLICATION_JSON)
-    public void modifySupplyOrder(@PathParam("id") Long id, String body) {
+    public Response modifySupplyOrder(@PathParam("id") Long id, String body) {
+        Response response;
         SupplyOrder supplyOrder = gson.fromJson(body, SupplyOrder.class);
-        supplyOrderBean.modify(id, supplyOrder);
+        if (supplyOrder == null) {
+            response = Response.status(Response.Status.BAD_REQUEST)
+                    .entity("Invalid Supply Order provided")
+                    .build();
+        } else {
+
+            supplyOrderBean.modify(id, supplyOrder);
+            response = Response.ok()
+                    .entity(supplyOrder)
+                    .type(MediaType.APPLICATION_JSON)
+                    .build();
+        }
+        return response;
     }
-    
+
     @DELETE
     @Path("{id}")
-    public void removeSupplyOrder(@PathParam("id") Long id) {
+    public Response removeSupplyOrder(@PathParam("id") Long id) {
+        Response response;
         supplyOrderBean.remove(id);
+        response = Response.status(Response.Status.ACCEPTED).build();
+        return response;
     }
-    
+
 }
