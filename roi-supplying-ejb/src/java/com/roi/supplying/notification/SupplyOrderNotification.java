594fd66b6b7749ec3c16ffb4be534631ffc9f5c0
pabmor <pabmorajau@gmail.com>
2018-06-21 17:18:32 -0200

    Started implementing HTTPClient to notify supply order state

--- roi-supplying-ejb/src/java/com/roi/supplying/notification/SupplyOrderNotification.java
+++ roi-supplying-ejb/src/java/com/roi/supplying/notification/SupplyOrderNotification.java
@@ -1,4 +1,4 @@
-package com.roi.supplying;
+package com.roi.supplying.notification;
 
 public class SupplyOrderNotification { 
  
--- roi-supplying-ejb/src/java/com/roi/supplying/notification/SupplyOrderNotificationBean.java
+++ roi-supplying-ejb/src/java/com/roi/supplying/notification/SupplyOrderNotificationBean.java
@@ -1,24 +1,77 @@
-package com.roi.supplying;
+package com.roi.supplying.notification;
 
+import com.roi.supplying.SupplyOrder;
 import javax.ejb.Stateless;
 import javax.ejb.LocalBean;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 @Stateless
 @LocalBean
 public class SupplyOrderNotificationBean {
-    //TODO remove maybe
+
     private SupplyOrderNotification createNotification(SupplyOrder supplyOrder) {
         long orderNumber = supplyOrder.getOrderNumber();
         long servicePointId = supplyOrder.getServicePointId();
-        
+
         return new SupplyOrderNotification(orderNumber, servicePointId);
     }
-    
+
     public void notifyCreation(SupplyOrder supplyOrder) {
         SupplyOrderNotification notification = createNotification(supplyOrder);
-        
-        //TODO send notification
+        String s = sendRequest("POST", "jsonObj");
+    }
+
+    public void notifyModification(SupplyOrder supplyOrder) {
+        SupplyOrderNotification notification = createNotification(supplyOrder);
+        String s = sendRequest("PUT", "jsonOBj");
+    }
+
+    public void notifyRemoval(SupplyOrder supplyOrder) {
+        SupplyOrderNotification notification = createNotification(supplyOrder);
+        sendRequest("DELETE", null);
+    }
+
+    public static String sendRequest(String method, String content) {
+        BufferedReader reader = null;
+        try {
+            URL url = new URL("Kremlin URL");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setReadTimeout(5000);
+            connection.setConnectTimeout(10000);
+            connection.setRequestMethod(method);
+            connection.setDoInput(true);
+            if (content != null) {
+                writeOutput(connection);
+            }
+            connection.connect();
+
+            reader = new BufferedReader(new InputStreamReader(connection.getInputStream(), "UTF-8"));
+            String valor = reader.readLine();
+            return valor;
+        } catch (IOException ex) {
+
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException ex) {
+
+                    return null;
+                }
+            }
+        }
+        return null;
+    }
+
+    private static void writeOutput(HttpURLConnection connection) throws IOException {
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(connection.getOutputStream()));
+        //Write JSON
     }
 }
-
-
