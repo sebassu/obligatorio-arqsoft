9a3936f2d4c3ee1c2df5194354d205d0635b3652
pabmor <pabmorajau@gmail.com>
2018-06-21 18:02:59 -0200

    Added comments with guidelines about future development

--- roi-planner-ejb/src/java/com/roi/planner/NotificationManagerBean.java
+++ roi-planner-ejb/src/java/com/roi/planner/NotificationManagerBean.java
@@ -7,15 +7,18 @@ public class NotificationManagerBean implements NotificationManagerBeanLocal {
 
     @Override
     public void created(SupplyOrderNotification notification) {
-        //TODO persist)? (with provided id) & create plan
-    
+        // TODO hit pipelinecalc and create Plan
+        // Send to Goliath
     }
     
     @Override
     public void modified(SupplyOrderNotification notification) {
+        // TODO if servicePointId changed hit pipelinecalc
+        // Then modify Plan
     }
     
     @Override
     public void removed() {
+        // TODO logically remove
     }
 }
