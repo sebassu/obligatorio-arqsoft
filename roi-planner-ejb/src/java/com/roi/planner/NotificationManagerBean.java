76d81859adc7d5d179a9562d343823a6b9c91a0f
pabmor <pabmorajau@gmail.com>
2018-06-22 16:22:47 -0200

    Implemented methods to handle notification. The only thing left is hit goliath

--- roi-planner-ejb/src/java/com/roi/planner/NotificationManagerBean.java
+++ roi-planner-ejb/src/java/com/roi/planner/NotificationManagerBean.java
@@ -1,32 +1,46 @@
 package com.roi.planner;
 
-import java.lang.reflect.Type;
 import java.util.ArrayList;
+import javax.ejb.EJB;
 import javax.ejb.Stateless;
 
 @Stateless
 public class NotificationManagerBean implements NotificationManagerBeanLocal {
 
+    @EJB
+    private SupplyPlanBean supplyPlanBean;
+    
     private static final String PIPELINE_URL = "https://pipeline-calculator-api.herokuapp.com/pipeline-route/service";
      
     @Override
     public void created(SupplyOrderNotification notification) {
-        String url = PIPELINE_URL + "/" + notification.getServicePointId();
-        ArrayList<NetworkFrame> networkFrames = (ArrayList<NetworkFrame>)Requester.
-                sendRequest(url, "POST", NetworkFrame.class, true);
-        SupplyPlan plan = SupplyPlan
-                .fromNotificationAndNetworkFrames(notification, networkFrames);
+        SupplyPlan plan = createPlan(notification);
+        supplyPlanBean.create(plan);
         //TODO Send to Goliath
     }
     
     @Override
     public void modified(SupplyOrderNotification notification) {
-        // TODO if servicePointId changed hit pipelinecalc
-        // Then modify Plan
+        SupplyPlan plan = supplyPlanBean.getByOrder(notification.getOrderNumber());
+        boolean changedServicePointId = plan.getServicePointId() != notification.getServicePointId();
+        if(changedServicePointId){
+            SupplyPlan newPlan = createPlan(notification);
+            supplyPlanBean.modify(newPlan, plan.getId());
+        }
     }
     
     @Override
-    public void removed() {
-        // TODO logically remove
+    public void removed(SupplyOrderNotification notification) {
+        SupplyPlan plan = supplyPlanBean.getByOrder(notification.getOrderNumber());
+        plan.setRemoved(true);
+        supplyPlanBean.modify(plan, plan.getId());
+    }
+    
+    private SupplyPlan createPlan(SupplyOrderNotification notification){
+        String url = PIPELINE_URL + "/" + notification.getServicePointId();
+        ArrayList<NetworkFrame> networkFrames = (ArrayList<NetworkFrame>)Requester.
+                sendRequest(url, "POST", NetworkFrame.class, true);
+        return SupplyPlan
+                .fromNotificationAndNetworkFrames(notification, networkFrames);
     }
 }
