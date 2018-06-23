97d82ce43bc8b28609077e07ef7ba76660ba8771
pabmor <pabmorajau@gmail.com>
2018-06-23 16:50:39 -0200

    Plan cant be changed if its already approved

--- roi-planner-ejb/src/java/com/roi/planner/NotificationManagerBean.java
+++ roi-planner-ejb/src/java/com/roi/planner/NotificationManagerBean.java
@@ -10,37 +10,37 @@ public class NotificationManagerBean implements NotificationManagerBeanLocal {
 
     @EJB
     private SupplyPlanBean supplyPlanBean;
-    
+
     private static final String PIPELINE_URL = "https://pipeline-calculator-api.herokuapp.com/pipeline-route/service";
-     
+
     @Override
     public void created(SupplyOrderNotification notification) {
         SupplyPlan plan = createPlan(notification);
         supplyPlanBean.create(plan);
-        //TODO Send to Goliath
     }
-    
+
     @Override
     public void modified(SupplyOrderNotification notification) {
         SupplyPlan plan = supplyPlanBean.getByOrder(notification.getOrderNumber());
+        boolean planIsNotYetApproved = plan.getStatus() != SupplyPlan.PlanStatus.APPROVED;
         boolean changedServicePointId = plan.getServicePointId() != notification.getServicePointId();
-        if(changedServicePointId){
+        if (planIsNotYetApproved && changedServicePointId) {
             SupplyPlan newPlan = createPlan(notification);
             supplyPlanBean.modify(newPlan, plan.getId());
         }
     }
-    
+
     @Override
     public void removed(long orderNumber) {
         SupplyPlan plan = supplyPlanBean.getByOrder(orderNumber);
         plan.setStatus(SupplyPlan.PlanStatus.REMOVED);
         supplyPlanBean.modify(plan, plan.getId());
     }
-    
-    private SupplyPlan createPlan(SupplyOrderNotification notification){
+
+    private SupplyPlan createPlan(SupplyOrderNotification notification) {
         String url = PIPELINE_URL + "/" + notification.getServicePointId();
-        ArrayList<NetworkFrame> networkFrames = (ArrayList<NetworkFrame>)Requester.
-                sendRequest(url, "POST", NetworkFrame.class, true);
+        ArrayList<NetworkFrame> networkFrames = (ArrayList<NetworkFrame>) Requester
+                .sendRequest(url, "POST", NetworkFrame.class, true);
         return SupplyPlan
                 .fromNotificationAndNetworkFrames(notification, networkFrames);
     }
