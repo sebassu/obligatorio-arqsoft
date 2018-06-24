531b618fa75b30e4e77d4680f3da70b7279fe94a
pabmor <pabmorajau@gmail.com>
2018-06-24 18:56:33 -0200

    Supplying inyeects token on every request

--- roi-supplying-ejb/src/java/com/roi/supplying/notification/SupplyOrderNotificationBean.java
+++ roi-supplying-ejb/src/java/com/roi/supplying/notification/SupplyOrderNotificationBean.java
@@ -1,14 +1,29 @@
 package com.roi.supplying.notification;
 
-import com.roi.http.Requester;
+import com.roi.http.Request;
+import com.roi.http.RequesterBean;
 import com.roi.supplying.SupplyOrder;
-import javax.ejb.Stateless;
+import com.roi.supplying.security.AuthenticationBean;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
 import javax.ejb.LocalBean;
+import javax.ejb.Stateful;
 
-@Stateless
+@Stateful
 @LocalBean
 public class SupplyOrderNotificationBean {
 
+    @EJB
+    private AuthenticationBean authenticationBean;
+    @EJB
+    private RequesterBean requesterBean;
+
+    @PostConstruct
+    private void init() {
+        token = authenticationBean.getToken().toString();
+    }
+
+    private String token;
     private static final String KREMLIN_URL = "Kremlin URL";
 
     private SupplyOrderNotification createNotification(SupplyOrder supplyOrder) {
@@ -20,20 +35,36 @@ public class SupplyOrderNotificationBean {
 
     public void notifyCreation(SupplyOrder supplyOrder) {
         SupplyOrderNotification notification = createNotification(supplyOrder);
-        String url = KREMLIN_URL;
-        String s = (String)Requester.sendRequest(url, "POST", String.class, false ,notification, SupplyOrderNotification.class);
+        Request request = new Request();
+        request.method = "POST";
+        request.url = KREMLIN_URL;
+        request.responseType = String.class;
+        request.responseIsList = false;
+        request.content = notification;
+        request.contentType = SupplyOrderNotification.class;
+        String s = (String) requesterBean.sendRequest(request);
     }
 
     public void notifyModification(SupplyOrder supplyOrder) {
         SupplyOrderNotification notification = createNotification(supplyOrder);
-        String url = KREMLIN_URL + "/" + supplyOrder.getOrderNumber();
-        String s = (String)Requester.sendRequest(url, "PUT", String.class, false, notification, SupplyOrderNotification.class);
+        Request request = new Request();
+        request.method = "PUT";
+        request.url = KREMLIN_URL + "/" + supplyOrder.getOrderNumber();
+        request.responseType = String.class;
+        request.responseIsList = false;
+        request.content = notification;
+        request.contentType = SupplyOrderNotification.class;
+        String s = (String) requesterBean.sendRequest(request);
     }
 
     public void notifyRemoval(SupplyOrder supplyOrder) {
         SupplyOrderNotification notification = createNotification(supplyOrder);
-        String url = KREMLIN_URL + "/" + supplyOrder.getOrderNumber();
-        String s = (String)Requester.sendRequest(url, "DELETE", String.class, false);
+        Request request = new Request();
+        request.method = "DELETE";
+        request.url = KREMLIN_URL + "/" + supplyOrder.getOrderNumber();
+        request.responseType = String.class;
+        request.responseIsList = false;
+        String s = (String) requesterBean.sendRequest(request);
     }
-    
+
 }
