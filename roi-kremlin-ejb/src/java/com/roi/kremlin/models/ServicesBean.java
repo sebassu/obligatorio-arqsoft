9e086996c9e5e3c1a15623f3f34deae2861adff8
pabmor <pabmorajau@gmail.com>
2018-06-25 12:32:37 -0200

    Files maybe need to be ignored

--- roi-kremlin-ejb/src/java/com/roi/kremlin/models/ServicesBean.java
+++ roi-kremlin-ejb/src/java/com/roi/kremlin/models/ServicesBean.java
@@ -8,7 +8,6 @@ import java.util.Arrays;
 import javax.ejb.Singleton;
 import javax.ejb.LocalBean;
 import java.util.List;
-import java.util.Map;
 import java.util.UUID;
 import javax.annotation.PostConstruct;
 
