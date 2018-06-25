fd33ef4ec104d0b0d8c5f6d353aab0511747a41a
pabmor <pabmorajau@gmail.com>
2018-06-25 21:14:02 -0200

    Created EAR for kremlin, fixed problems

--- roi-kremlin-war/src/java/com/roi/kremlin/FunctionsResource.java
+++ roi-kremlin-war/src/java/com/roi/kremlin/FunctionsResource.java
@@ -1,6 +1,8 @@
 package com.roi.kremlin;
 
 import com.roi.kremlin.models.AuthorizationBean;
+import com.roi.kremlin.models.ServicesBean;
+import com.roi.kremlin.models.ServicesBean;
 import com.roi.kremlin.models.ValidatorBean;
 import java.util.List;
 import javax.ws.rs.Consumes;
@@ -8,23 +10,32 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
 import javax.ejb.EJB;
+import javax.ws.rs.GET;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.Response;
 
 @Path("functions")
 public class FunctionsResource {
+    
+    @EJB
+    private AuthorizationBean authorizationBean;
 
     @EJB
-    AuthorizationBean authorizationBean;
+    private ValidatorBean validatorBean;
 
-    @EJB
-    ValidatorBean validatorBean;
-
+    @GET
+    public Response removeSupplyOrder() {
+        Response response;
+        response = Response.status(Response.Status.OK).build();
+        return response;
+    }
+    
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Path("{appName}/{function}")
-    public Response call(String appName, String function, String content, @Context HttpHeaders headers) {
+    public Response call(@PathParam("appName")String appName, @PathParam("function")String function, String content, @Context HttpHeaders headers) {
 
         List<String> authHeaders = headers.getRequestHeader(HttpHeaders.AUTHORIZATION);
         if (authHeaders.isEmpty()) {
